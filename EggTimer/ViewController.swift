4d7385028b734c9a9cf80ae5034b4f56c10d21fd
Ingrid von Baranow <ingrid_baranow@hotmail.com>
2025-04-14 14:43:41 -0300

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Ingrid Baranow on 14/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
